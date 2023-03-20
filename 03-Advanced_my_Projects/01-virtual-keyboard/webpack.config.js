ef7f73d164a83e724a53beb20d3eaab8442c9529
EggsyJ <58285674+JeonChanMin@users.noreply.github.com>
2023-03-20 17:43:29 +0900

    Install TerserPlugin and Css-Minizer-Webpack-Plugin, Set webpack-config-js

--- 03-Advanced_my_Projects/01-virtual-keyboard/webpack.config.js
+++ 03-Advanced_my_Projects/01-virtual-keyboard/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -34,5 +36,9 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
 };
