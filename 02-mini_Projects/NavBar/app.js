f88461684397799efc06e8e28933665d97437b78
EggsyJ <58285674+JeonChanMin@users.noreply.github.com>
2023-03-21 00:36:40 +0900

    Delete mini-projects

--- 02-mini_Projects/NavBar/app.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-const links = document.querySelector(".links");
-
-navToggle.addEventListener("click", () => {
-  links.classList.toggle("show-links");
-});
