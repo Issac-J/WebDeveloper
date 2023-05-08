fd1ff5945606921c01f6740b7d013e4e61b23776
EggsyJ <58285674+JeonChanMin@users.noreply.github.com>
2023-05-08 12:27:04 +0900

    Deleted files

--- 02-mini_Projects/01-colorflipper/src/js/index.js
+++
@@ -1,6 +0,0 @@
-import "../css/style.css";
-import { ColorFlipper } from "./colorFlipper.js";
-import { HexFlipper } from "./hexFlipper.js";
-
-new ColorFlipper();
-new HexFlipper();
--- 02-mini_Projects/02-counterBtn/src/js/index.js
+++
@@ -1,4 +0,0 @@
-import "../css/style.css";
-import { Counter } from "./counter.js";
-
-new Counter();
