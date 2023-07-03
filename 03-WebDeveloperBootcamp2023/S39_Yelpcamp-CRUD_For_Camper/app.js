8cf122c1a0102a831d66466eed4573734777cfc8
EggsyJ <58285674+JeonChanMin@users.noreply.github.com>
2023-07-03 22:05:03 +0900

    Complete New, Create, Edit, Update Function

--- 03-WebDeveloperBootcamp2023/S39_Yelpcamp-CRUD_For_Camper/app.js
+++ 03-WebDeveloperBootcamp2023/S39_Yelpcamp-CRUD_For_Camper/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const methodOverride = require("method-override");
+
 const Campground = require("./models/campground");
 
 // Declare Mongoose
@@ -18,6 +20,9 @@ db.once("open", () => {
 // Setting Ejs
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -38,10 +43,36 @@ app.get("/campgrounds", async (req, res) => {
   res.render("campgrounds/index", { campgrounds });
 });
 
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
 app.get("/campgrounds/:id", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
 
   res.render("campgrounds/show", { campground });
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.listen(3000, () => {
