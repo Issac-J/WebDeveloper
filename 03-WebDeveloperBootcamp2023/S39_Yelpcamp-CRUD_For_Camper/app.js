4a65a58f5aa36da2a83e66128479f50f958e49a5
EggsyJ <58285674+JeonChanMin@users.noreply.github.com>
2023-07-03 20:59:04 +0900

    Add show in campgrounds

--- 03-WebDeveloperBootcamp2023/S39_Yelpcamp-CRUD_For_Camper/app.js
+++ 03-WebDeveloperBootcamp2023/S39_Yelpcamp-CRUD_For_Camper/app.js
@@ -38,6 +38,12 @@ app.get("/campgrounds", async (req, res) => {
   res.render("campgrounds/index", { campgrounds });
 });
 
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+
+  res.render("campgrounds/show", { campground });
+});
+
 app.listen(3000, () => {
   console.log("Serving on Port: 3000");
 });
